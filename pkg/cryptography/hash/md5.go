9f1fa12b99ee4cd3f51cb05f1a5ec3e8c9b1841b
EternallyAscend <Eternally-Ascend@Outlook.com>
2022-04-04 20:27:50 +0800

    Add md5 functions.

---
+++ pkg/cryptography/hash/md5.go
@@ -0,0 +1,20 @@
+package hash
+
+import "crypto/md5"
+
+// md5 https://www.jianshu.com/p/5b6f7110eb52
+
+func MD5(value []byte) []byte {
+	hashValue := md5.New()
+	hashValue.Write(value)
+	hashResult := hashValue.Sum(nil)
+	return hashResult
+}
+
+func MD5String(value []byte) string {
+	return string(MD5(value))
+}
+
+func MD5Verify(value []byte, hashValue string) bool {
+	return MD5String(value) == hashValue
+}
